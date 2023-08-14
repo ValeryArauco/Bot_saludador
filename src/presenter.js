6aa6049c4fa8bbd873f437bb970c63a74f63adf4
ValeryArauco <141700272+ValeryArauco@users.noreply.github.com>
2023-08-14 19:57:03 -0400

    Saludar por nombre

--- src/presenter.js
+++ src/presenter.js
@@ -1,23 +1,23 @@
-// import saludar from "./saludar";
+import saludar from "./saludar";
 
 
-// const name = document.querySelector("#nombre");
+const name = document.querySelector("#nombre");
 // // const second = document.querySelector("#segundo-numero");
 // // const first1 = document.querySelector("#first-number");
 // // const second1 = document.querySelector("#second-number");
 // // const form = document.querySelector("#sumar-form");
 // // const form1 = document.querySelector("#multiplicar-form");
-// const div = document.querySelector("#resultado-div");
-// const form = document.querySelector("#saludar-form");
+const div = document.querySelector("#resultado-div");
+const form = document.querySelector("#saludar-form");
 
-// form.addEventListener("submit", (event) => {
-//   event.preventDefault();
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
 
-//   const nombre = Text(name);
+   const nombre = name.value;
 
-//   div.innerHTML = "<p>" + saludar(nombre) + "</p>";
+  div.innerHTML = "<p>" + saludar(nombre) + "</p>";
 
-// });
+});
 
 // form1.addEventListener("submit", (event) => {
 //   event.preventDefault();
