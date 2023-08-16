419a1f3b1cb61f0e3747aa380eea0621a04bb0f0
ValeryArauco <141700272+ValeryArauco@users.noreply.github.com>
2023-08-16 14:49:03 -0400

    Test para saludar por genero

--- src/saludar.test.js
+++ src/saludar.test.js
@@ -10,12 +10,16 @@ describe("Saludar por nombre", () => {
     
 });
 
-// describe("Saludar por nombre", () => {
-//     it("deberia Saludar a Andrés ", () => {
-//         let kbot = new Kbot();
-//         let resultado = kbot.saludar("Andrés", 7) 
-//         expect(resultado).toEqual("Buenos días Andrés");
-//     });
+describe("Saludar de acuerdo al genero", () => {
+    it("deberia Saludar a Andrés ", () => {
+        
+        let resultado = saludar("Andrés", "masculino", 45, 8, "es") 
+        expect(resultado).toEqual("Buenos días, Sr. Andrés.");
+    });
 
-    
-// });
+    it("deberia Saludar a María ", () => {
+        
+        let resultado = saludar("María", "femenino", 45, 8, "es") 
+        expect(resultado).toEqual("Buenos días, Sra. María.");
+    });
+});
