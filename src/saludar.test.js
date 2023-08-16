5ad5856e8e6a411229fd85190b482e225ba4d843
ValeryArauco <141700272+ValeryArauco@users.noreply.github.com>
2023-08-16 15:10:55 -0400

    Test para saludar de acuerdo a la hora del día en ingles

--- src/saludar.test.js
+++ src/saludar.test.js
@@ -23,3 +23,25 @@ describe("Saludar de acuerdo al genero", () => {
         expect(resultado).toEqual("Buenos días, Sra. María.");
     });
 });
+
+
+describe("Saludar de acuerdo a la hora del día en ingles", () => {
+    it("deberia Saludar a Andrés en la mañana en ingles", () => {
+        
+        let resultado = saludar("Andrés", "masculino", 45, 8, "en") 
+        expect(resultado).toEqual("Good morning, Mr. Andrés.");
+    });
+
+    it("deberia Saludar a Andrés en la tarde en ingles", () => {
+        
+        let resultado = saludar("Andrés", "masculino", 45, 15, "en") 
+        expect(resultado).toEqual("Good afternoon, Mr. Andrés.");
+    });
+
+    it("deberia Saludar a Andrés en la noche en ingles", () => {
+        
+        let resultado = saludar("Andrés", "masculino", 45, 21, "en") 
+        expect(resultado).toEqual("Good evening, Mr. Andrés.");
+    });
+
+});
